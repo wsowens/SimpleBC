85eb897934a6ba5791aa7d6610bbb35cc7f7ea73
xeranimus <wowens@ufl.edu>
2019-02-07 12:27:20 -0500

    added new testing script

--- src/test-all.sh
+++ src/test-all.sh
@@ -10,9 +10,13 @@
 export CLASSPATH=".:/usr/local/lib/antlr-4.7.1-complete.jar:$CLASSPATH"
 
 # Default values for testing files
-relative_path_test_directory=$'../test/'
+top_test_directory=$'../test/'
 input_file_suffix=$'-input.bc'
-output_file_suffix=$'-output.txt'
+expected=$'expected.txt'
+actual=$'actual.txt'
+tree=$'tree.txt'
+errors=$'errors.txt'
+diff=$'diff.txt'
 
 # Reference (non-functioning) code for running and compiling the code
 # antlr4=(java -Xmx500M -cp "/usr/local/lib/antlr-4.7.1-complete.jar:$CLASSPATH" org.antlr.v4.Tool)
@@ -21,7 +25,7 @@ output_file_suffix=$'-output.txt'
 
 # The will output if the files failed or passed
 diffFiles() {
-	if diff <(java -Xmx500M -cp $CLASSPATH org.antlr.v4.gui.TestRig SimpleBC exprList $1) $2 > /dev/null
+	if diff -y $1 $2 > $3
 	then
 			echo "Pass: " $1
 	else
@@ -30,9 +34,20 @@ diffFiles() {
 }
 
 # Go through all the files in the test directory that match the input suffix
-for file in $relative_path_test_directory*$input_file_suffix
+for file in $top_test_directory*$input_file_suffix
 do
-	# Compare the input files output with the output file
-	diffFiles $file ${file%$input_file_suffix}$output_file_suffix
-done
+	test_dir="$top_test_directory$(basename -s $input_file_suffix $file)"
+	mkdir -p $test_dir
+	# run the test file through bc, make output the expected
+	bc -l < $file > "${test_dir}/$expected"	
+	# running the generated + compiled parser on each test file
+	java -Xmx500M -cp $CLASSPATH org.antlr.v4.gui.TestRig SimpleBC exprList -tree $file > "${test_dir}/temp" 2> "${test_dir}/$errors"
+	
+	# separating the tree from the main expression
+	head -n-1 "${test_dir}/temp" > "${test_dir}/${actual}"
+	tail -n 1 "${test_dir}/temp" > "${test_dir}/${tree}"
+	rm "${test_dir}/temp"
 
+	# Compare the input files output with the output file
+	diffFiles "${test_dir}/${actual}" "${test_dir}/$expected" "${test_dir}/${diff}"
+done
